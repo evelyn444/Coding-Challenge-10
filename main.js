9e5879701737708396b1acf64036cc9f4f40b7fb
Evelyn_ Hernandez <eh22@usf.edu>
2024-10-20 21:09:00 -0400

    Add event listener to update price on size selection

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //Task 2:Add Event Listeners for Product Selection
+const priceElement = document.getElementById('product-price');
+const modelSelector = document.getElementById ('model-selector');
+const purchaseButton = document.getElementById ('purchase-button');
+
+modelSelector.addEventListener('change', function(event){ // changes price on screen depending what model customer chooses
+    const selectedOption = modelSelector.options[modelSelector.selectedIndex];
+    const selectedPrice = event.target.value;
+    priceElement.textContent = `$${selectedPrice}`; 
+}
+);
